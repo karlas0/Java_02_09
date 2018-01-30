0156a4e10d08dab31b0861d5b46ca4978714f7fa
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-30 16:49:00 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    static float a;
+    static float b;
+
     public static void main(String[] args) {
-	// write your code here
+        nuskaitymas();
+
+        System.out.println("Jusu vidurkis");
+
+        System.out.println(skaiciavimas());
+
     }
+
+    public static void nuskaitymas() {
+        System.out.println("Sveiki, iveskite nuvaziuotus km");
+
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextFloat();
+
+        System.out.println("Iveskite sunaudotu degalu kieki");
+
+        b = sc.nextFloat();
+    }
+
+
+    public static float skaiciavimas() {
+        return b * (100 / a);
+    }
+
+
+
 }
